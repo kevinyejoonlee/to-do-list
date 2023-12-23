e193fcdd70487aa0d41c88a0a573e3928406139d
Kevin Yejoon Lee <73869929+kevinyejoonlee@users.noreply.github.com>
2023-12-22 19:47:47 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+const maxItems = 7;
+var count = 0;
+var tasks = {}
+
+
+app.use(express.static('public')); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) =>{
+    res.render("index.ejs", {
+        ToDo: tasks,
+        items: Object.keys(tasks).length
+    });
+})
+
+app.post("/submit", (req, res) =>{
+    var receivedText = req.body.InputText;
+
+    if (Object.keys(tasks).length < 7){
+        count ++;
+        tasks[count] = receivedText ;
+    }
+    res.status(303).redirect('/');
+});
+
+app.post("/delete", (req, res) =>{
+    var recivedItem = req.body.trash;
+    delete tasks[recivedItem];
+    res.status(303).redirect('/');
+});
+
+
+app.listen(port, () => {
+    console.log(`http://localhost:3000/`);
+  });
+  
+
